59408c3a1c8cb026da68c348d5a8ed9848ff3383
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-11-27 00:38:39 +0600

    seller options added

---
+++ src/pages/Dashboard/Dashboard/Saller/AddAProduct/AddAProduct.js
@@ -0,0 +1,189 @@
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../../../Context/AuthProvider';
+
+
+const AddAProduct = () => {
+    const categories = useLoaderData();
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const location = form.location.value;
+        const used = form.used.value;
+        const resalePrice = form.resalePrice.value;
+        const originalPrice = form.originalPrice.value;
+        const category = form.category.value;
+        const image = form.image.files[0];
+        const salerName = form.salerName.value;
+
+
+
+
+
+        // console.log(product);
+
+
+        const formData = new FormData()
+        formData.append('image', image)
+
+        const url = 'https://api.imgbb.com/1/upload?key=c993754e5e7bdf8ca9412defbbd79642'
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+        })
+            .then(res => res.json())
+            .then(imageData => {
+
+                if (imageData.success) {
+
+                    const product = {
+                        salerName,
+                        category,
+                        originalPrice,
+                        resalePrice,
+                        location,
+                        used,
+                        photo: imageData.data.url,
+                        name,
+                        email: user?.email
+                    }
+                    // console.log(userInfo)
+                    // save doctor information to the database
+
+                    fetch('http://localhost:5000/add-a-product', {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json',
+                            // authorization: `bearer ${localStorage.getItem('accessToken')}`
+                        },
+                        body: JSON.stringify(product)
+                    })
+                        .then(res => res.json())
+                        .then(result => {
+                            if (result.acknowledged) {
+                                toast.success('Your Product Has Added Successfully');
+                                navigate('/dashboard/my-products')
+                            }
+                            console.log(result)
+                        })
+                }
+            })
+
+    }
+
+
+
+    return (
+        <div >
+            <div className="bg-indigo-50">
+                <form onSubmit={handleSubmit} className="xl:px-20 md:px-10 sm:px-6 px-4 md:py-12 py-9 2xl:mx-auto 2xl:container md:flex items-center justify-center">
+
+                    <div className="bg-white shadow-lg rounded  md:w-1/2 w-full lg:px-10 sm:px-6 sm:py-10 px-2 py-6">
+                        <p tabIndex={0} className="focus:outline-none text-2xl font-extrabold leading-6 text-gray-800">
+                            Add Your Product
+                        </p>
+
+
+
+
+                        <div className="mt-6 w-full">
+                            <label htmlFor="name" className="text-sm font-medium leading-none text-gray-800">
+                                {" "}
+                                Product Name{" "}
+                            </label>
+                            <input name='name' id="name" aria-labelledby="name" type="text" className="bg-gray-200 border rounded text-xs font-medium leading-none placeholder-gray-800 text-gray-800 py-3 w-full pl-3 mt-2 " placeholder="e.g: I Phone 14 pro Max xx " required />
+                        </div>
+                        <div className="mt-6 w-full">
+                            <label htmlFor="image" className="text-sm font-medium leading-none text-gray-800">
+                                {" "}
+                                Select Image{" "}
+                            </label>
+                            <br />
+                            <input
+                                className='mt-2 mb-5'
+                                type='file'
+                                id='image'
+                                name='image'
+                                accept='image/*'
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="category" className="text-sm font-medium leading-none text-gray-800">
+                                {" "}
+                                Select Brand{" "}
+                            </label>
+                            <select name='category' id='category' className="select select-success w-full my-2" required>
+
+
+                                {
+                                    categories.map(category => <option
+                                        key={category._id}
+                                        value={category.name}
+                                    >{category.name}</option>)
+                                }
+
+                            </select>
+                        </div>
+
+                        <div className="mt-6 w-full">
+                            <label htmlFor="location" className="text-sm font-medium leading-none text-gray-800">
+                                {" "}
+                                Location{" "}
+                            </label>
+                            <input name='location' id="location" aria-labelledby="location" type="text" className="bg-gray-200 border rounded text-xs font-medium leading-none placeholder-gray-800 text-gray-800 py-3 w-full pl-3 mt-2 " placeholder="e.g: Dhaka, Bangladesh " required />
+                        </div>
+
+                        <div className="mt-6 w-full">
+                            <label htmlFor="used" className="text-sm font-medium leading-none text-gray-800">
+                                {" "}
+                                How many years/months You Used?{" "}
+                            </label>
+                            <input name='used' id="used" aria-labelledby="used" type="text" className="bg-gray-200 border rounded text-xs font-medium leading-none placeholder-gray-800 text-gray-800 py-3 w-full pl-3 mt-2 " placeholder="e.g: 1 year / 6 months " required />
+                        </div>
+
+
+                        <div className="mt-6 w-full">
+                            <label htmlFor="originalPrice" className="text-sm font-medium leading-none text-gray-800">
+                                {" "}
+                                Original Price{" "}
+                            </label>
+                            <input name='originalPrice' id="originalPrice" aria-labelledby="originalPrice" type="text" className="bg-gray-200 border rounded text-xs font-medium leading-none placeholder-gray-800 text-gray-800 py-3 w-full pl-3 mt-2 " placeholder="e.g: 12,000 " required />
+                        </div>
+
+
+                        <div className="mt-6 w-full">
+                            <label htmlFor="resalePrice" className="text-sm font-medium leading-none text-gray-800">
+                                {" "}
+                                Sale Price{" "}
+                            </label>
+                            <input name='resalePrice' id="resalePrice" aria-labelledby="resalePrice" type="text" className="bg-gray-200 border rounded text-xs font-medium leading-none placeholder-gray-800 text-gray-800 py-3 w-full pl-3 mt-2 " placeholder="e.g: 12,000 " required />
+                        </div>
+
+                        <div className="mt-6 w-full">
+                            <label htmlFor="salerName" className="text-sm font-medium leading-none text-gray-800">
+                                {" "}
+                                Your Name{" "}
+                            </label>
+                            <input name='salerName' disabled defaultValue={user?.displayName} id="salerName" aria-labelledby="salerName" type="text" className="bg-gray-200 border rounded text-xs font-medium leading-none placeholder-gray-800 text-gray-800 py-3 w-full pl-3 mt-2 " placeholder="e.g: Md Shijan Ali " required />
+                        </div>
+
+
+                        <input className='w-full btn btn-primary mt-10' type="submit" value="Submit" />
+
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default AddAProduct;
---
+++ src/pages/Dashboard/Dashboard/Saller/MyBuyers/MyBuyers.js
@@ -0,0 +1,73 @@
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../../../Context/AuthProvider";
+
+const MyBuyers = () => {
+  const { user } = useContext(AuthContext);
+
+  const { data: myBuyers = [] } = useQuery({
+    queryKey: ["myBuyers"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(
+          `http://localhost:5000/my-orders/${user?.email}`
+        );
+        const data = await res.json();
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+  return (
+    <div>
+      <h2 className="text-3xl text-center font-semibold  mt-10">My Buyers</h2>
+
+      {myBuyers.length === 0 ? (
+        <h2 className="text-3xl font-semibold mt-10 text-center">
+          You Have not any Buyers
+        </h2>
+      ) : (
+        <div className="overflow-x-auto w-5/6 mx-auto my-10">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th>Avater</th>
+                <th>Buyer Name</th>
+                <th>Location</th>
+                <th>Phone Number</th>
+              </tr>
+            </thead>
+            <tbody>
+              {myBuyers.map((order) => (
+                <tr key={order._id}>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            src={order?.buyerPhoto}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div>
+                      <div className="font-bold">{order.buyerName}</div>
+                    </div>
+                  </td>
+                  <td>{order.location}</td>
+                  <td>{order.phone}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyBuyers;
